47c36814a8e7c6f0ba304fdf3816099d470e39aa
ben avrahami <ben.avrahami@biocatch.com>
2021-01-27 17:22:12 +0200

    wf fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,7 @@
 # install the dev-dependencies of the project
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | POETRY_HOME=/opt/poetry python && \
+    cd /usr/local/bin && \
+    ln -s /opt/poetry/bin/poetry
+
 poetry update --lock
 poetry install
