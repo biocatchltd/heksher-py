aab1246ff440a3ba0fafacc5ba9f7880e32f6ed0
ben avrahami <ben.avrahami@biocatch.com>
2021-12-05 15:59:08 +0200

    lint

--- heksher/__init__.py
+++ heksher/__init__.py
@@ -1,7 +1,7 @@
+from heksher._version import __version__
 from heksher.clients.async_client import AsyncHeksherClient
 from heksher.clients.subclasses import TRACK_ALL
 from heksher.clients.thread_client import ThreadHeksherClient
 from heksher.setting import Setting
-from heksher._version import __version__
 
 __all__ = ['AsyncHeksherClient', 'TRACK_ALL', 'ThreadHeksherClient', 'Setting', '__version__']
