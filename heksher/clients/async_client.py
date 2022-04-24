159afc30281bd47bca8e2c9d568ee90d1308c592
ben avrahami <ben.avrahami@biocatch.com>
2022-04-24 18:06:29 +0300

    fixes

--- heksher/clients/async_client.py
+++ heksher/clients/async_client.py
@@ -70,9 +70,9 @@ class AsyncHeksherClient(V1APIClient, ContextFeaturesMixin, AsyncContextManagerM
         """
 
         async def declare_setting(setting):
-            response = await self._http_client.put('api/v1/settings/declare',
-                                                   content=orjson.dumps(setting.to_v1_declaration_body()),
-                                                   headers=content_header)
+            response = await self._http_client.post('api/v1/settings/declare',
+                                                    content=orjson.dumps(setting.to_v1_declaration_body()),
+                                                    headers=content_header)
             self._handle_declaration_response(setting, response)
 
         while True:
