48c0eceaf78fc986145611cfb7cabd5611579cc6
razi434 <70629934+razi434@users.noreply.github.com>
2022-08-22 13:34:39 +0300

    .

--- tests/blackbox/app/test_main.py
+++ tests/blackbox/app/test_main.py
@@ -1,8 +1,12 @@
+from asyncio import sleep
+from logging import WARNING
+
 from pytest import mark
 from yellowbox_heksher.heksher_service import HeksherService
 
 from heksher import AsyncHeksherClient, Setting
 from tests.blackbox.app.utils import CreateRuleParams
+from tests.unittest.util import assert_logs
 
 
 @mark.asyncio
@@ -62,4 +66,49 @@ async def test_declare_after_main(heksher_service, monkeypatch, add_rules):
     await add_rules([CreateRuleParams(setting='cache_size', feature_values={'b': '5'}, value=100)])
     await client.reload()
     assert setting.get(b='5', c='') == 100
+    await client.aclose()
+
+
+@mark.asyncio
+async def test_regular_update(heksher_service, monkeypatch, add_rules):
+    setting = Setting('cache_size', type=int, configurable_features=['b', 'c'], default_value=50)
+
+    client = AsyncHeksherClient(heksher_service.local_url, 0.02, ['a', 'b', 'c'])
+    await client.set_as_main()
+    client.track_contexts(b='0')
+
+    await add_rules([CreateRuleParams(setting='cache_size', feature_values={'b': '0'}, value=100)])
+    await sleep(1)
+    assert setting.get(b='0', c='') == 100
+    await client.aclose()
+
+
+@mark.asyncio
+@mark.parametrize('expected', [
+    [],
+    ['a'],
+    ['b', 'a'],
+    ['a', 'b', 'c', 'd'],
+    ['e', 'f', 'g']
+])
+async def test_cf_mismatch(heksher_service, caplog, monkeypatch, expected):
+    print(f"!!!!{expected=}")
+    with assert_logs(caplog, WARNING):
+        async with AsyncHeksherClient(heksher_service.local_url, 1000, expected) as client:
+            assert client._context_features == expected
+
+
+@mark.asyncio
+async def test_redundant_defaults(heksher_service, add_rules, caplog):
+    setting = Setting('cache_size', type=int, configurable_features=['b', 'c'], default_value=50)
+    client = AsyncHeksherClient(heksher_service.local_url, 10000000, ['a', 'b', 'c'])
+    await client.set_as_main()
+
+    client.track_contexts(b='B')
+    await add_rules([CreateRuleParams(setting='cache_size', feature_values={'b': 'B'}, value=100)])
+    await client.reload()
+
+    with assert_logs(caplog, WARNING):
+        client.set_defaults(b='B', d='im redundant')
+        assert setting.get(c='') == 100
     await client.aclose()
